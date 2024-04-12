331a0718682ad3538439110a6099718ea9776945
Claus Reinhold <claus.reinhold@gmail.com>
2024-04-12 14:47:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# nextjs-shadcn-bash-script
+
+```bash
+~/create-next-app.sh <project-name>
+```
+
+### What does the script do
+
+1. Install the latest version of next.js
+2. Install the latest version of ShadCn
+3. Adds Button and Input component
+4. Install prettier, eslint-config-prettier, prettier-plugin-tailwindcss
+5. Create a .prettierrc file with default configuration
+6. Updated the .eslintrc with prettier
+7. Adds format and format:fix to package.json script
+8. runs npm run format:fix
+9. Enjoy your new Next.js project. 🎉
---
+++ create-next-app.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Function to update JSON files using sed
+update_json() {
+  file="$1"
+  tmpfile=$(mktemp)
+  sed 's/\("extends": \)\("[^"]*"\)/\1\[\2, "prettier"\]/' "$file" >"$tmpfile" && mv "$tmpfile" "$file"
+}
+
+# Check if a project name is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <project_name>"
+  exit 1
+fi
+# Use the provided project name or default to "my-next-app"
+project_name="${1:-my-next-app}"
+
+# Create a new Next.js app with the provided project name
+yes "" | npx create-next-app@latest "$project_name"
+
+# Wait for the project directory to be created
+while [ ! -d "$project_name" ]; do
+  echo "Waiting for project directory to be created..."
+  sleep 1
+done
+
+echo "Project Name: $project_name"
+
+# Navigate into the project directory
+cd "$project_name" || exit
+
+# Install Shadcn
+yes "" | npx shadcn-ui@latest init
+
+# Install few components from Shadcn
+npx shadcn-ui@latest add button
+npx shadcn-ui@latest add input
+
+# Install prettier and eslint plugin
+npm install --save-dev prettier eslint-config-prettier prettier-plugin-tailwindcss
+
+# Create a .prettierrc file with default configuration
+echo '{
+  "trailingComma": "es5",
+  "semi": false,
+  "tabWidth": 2,
+  "singleQuote": true,
+  "jsxSingleQuote": true,
+  "plugins": ["prettier-plugin-tailwindcss"]
+}' >.prettierrc
+
+# Modifies the eslintrc.json file to extend prettier
+update_json .eslintrc.json
+
+# Modify package.json to include format scripts
+sed -i '/"scripts": {/a\
+  "format": "prettier --check --ignore-path .gitignore .",\
+  "format:fix": "prettier --write --ignore-path .gitignore .",\
+' package.json
+
+# Run the format:fix script to format the project files
+npm run format:fix
+
+# Finish the setup
+echo "Setup complete! Navigate to the project directory with 'cd $project_name' and start the development server with 'npm run dev'."
+
+echo "You might still want to edit the layout file according to your needs and shadcn docs"
